20293bd784585128d05e4cdc0c58e0bd2bd4e6f6
arvids-dane <arvids.dane@gmail.com>
2019-08-29 10:57:34 +0300

    Pabeigts otrais uzdevums par masīva elementu ievadi/dzēšanu un citām lietām

--- uzdevumi/uzd2/Scripts/script.js
+++ uzdevumi/uzd2/Scripts/script.js
@@ -4,7 +4,7 @@ function addElement(){
     let vertiba = document.getElementById("masElements").value;
     vertiba == "" ? window.alert("Lūdzu, aizpildi ievadlauciņu!") : masivs.push(vertiba);
     document.getElementById("masElements").value="";
-    printOutArrCntnt()
+    printOutArrCntnt();
 }
 function deleteElement(){
     //let indekss = parseInt(document.getElementById("masElemIndx").value);
@@ -14,16 +14,17 @@ function deleteElement(){
     (masivs.length == 0 ? //Aplūkojam vai masīvs nav galīgi tukšs
          window.alert("Tukšs masīvs, lūdzu, ielādē kādu elementu!") : "") : masivs.splice(indekss-1, 1);
     document.getElementById("masElemIndx").value=""; //Attīru ievadlauciņu
-    printOutArrCntnt()
+    printOutArrCntnt();
 }
 function addDesiredElement(){
      //let vertiba = parseInt(document.getElementById("masElements").value);
      let vertiba = document.getElementById("masivaElements").value;
-     vertiba == "" ? window.alert("Lūdzu, aizpildi ievadlauciņu!") : masivs.push(vertiba);
+     let vieta = document.getElementById("indekss").value;
+     vertiba == "" ? window.alert("Lūdzu, aizpildi ievadlauciņu!") : masivs.splice(vieta,0,vertiba);
      document.getElementById("masivaElements").value="";
-     printOutArrCntnt()
+     document.getElementById("indekss").value="";
+     printOutArrCntnt();
  }
-}
 function printOutArrCntnt(){
     let izvads = "";
     for(let i = 0; i < masivs.length; i++){
